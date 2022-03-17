eee0240d6224d184bcacab0f79b315d66f4eb4c5
Mathias <mat.micheli99@gmail.com>
2022-03-17 17:51:06 +0100

    feat: add Note entity

---
+++ src/Entity/Note.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NoteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NoteRepository::class)]
+class Note
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'text')]
+    private $note;
+
+    #[ORM\Column(type: 'date')]
+    private $date;
+
+    #[ORM\ManyToOne(targetEntity: Kid::class, inversedBy: 'notes')]
+    private $kid;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNote(): ?string
+    {
+        return $this->note;
+    }
+
+    public function setNote(string $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getKid(): ?Kid
+    {
+        return $this->kid;
+    }
+
+    public function setKid(?Kid $kid): self
+    {
+        $this->kid = $kid;
+
+        return $this;
+    }
+}
