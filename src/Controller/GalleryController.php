0a316da4faf9bfca63b38d0214611c0618148f76
Mathias <mat.micheli99@gmail.com>
2022-04-04 20:23:40 +0200

    feat: add gallery config and routes

---
+++ src/Controller/GalleryController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Gallery;
+use App\Repository\GalleryRepository;
+use App\Repository\NurseRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class GalleryController extends AbstractController
+{
+    private SluggerInterface $slugger;
+    private NurseRepository $nurseRepository;
+    private ManagerRegistry $doctrine;
+    private GalleryRepository $galleryRepository;
+
+    public function __construct(SluggerInterface  $slugger,
+                                NurseRepository   $nurseRepository,
+                                ManagerRegistry   $doctrine,
+                                GalleryRepository $galleryRepository)
+    {
+        $this->slugger = $slugger;
+        $this->nurseRepository = $nurseRepository;
+        $this->doctrine = $doctrine;
+        $this->galleryRepository = $galleryRepository;
+    }
+
+    #[Route('/gallery/{nurseId}', name: 'app_gallery', methods: 'POST')]
+    public function index(Request $request, int $nurseId): JsonResponse
+    {
+        $files = $request->files;
+        $nurse = $this->nurseRepository->findOneBy(['id' => $nurseId]);
+        $entityManager = $this->doctrine->getManager();
+
+        /* @var UploadedFile $file */
+        foreach ($files as $file) {
+            $safeFilename = $this->slugger->slug($file->getClientOriginalName());
+            $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+            $photo = (new Gallery())->setUrl($fileName)->setNurse($nurse);
+            $entityManager->persist($photo);
+            $entityManager->flush();
+
+            try {
+                $file->move($this->getParameter('gallery_directory').'/'.$nurseId, $fileName);
+            } catch (FileException $e) {
+                throw new \Exception($e->getMessage());
+            }
+        }
+
+        return $this->json([], Response::HTTP_CREATED);
+    }
+
+    #[Route('/gallery/nurse/{nurseId}', name: 'app_gallery_get', methods: 'GET')]
+    public function gallery(int $nurseId): JsonResponse
+    {
+        $photos = $this->galleryRepository->findBy(['nurse' => $nurseId]);
+        return $this->json($photos, Response::HTTP_CREATED, [], ['groups' => 'gallery']);
+    }
+}
