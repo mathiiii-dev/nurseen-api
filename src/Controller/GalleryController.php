f1e26b08252be8f49dfa4eece93595c12efe9df1
Mathias <mat.micheli99@gmail.com>
2022-04-05 18:41:21 +0200

    feat: add delete image

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -6,6 +6,7 @@ use App\Entity\Gallery;
 use App\Repository\GalleryRepository;
 use App\Repository\NurseRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
@@ -33,6 +34,7 @@ class GalleryController extends AbstractController
         $this->galleryRepository = $galleryRepository;
     }
 
+    #[IsGranted('ROLE_NURSE', message: 'Vous ne pouvez pas faire ça')]
     #[Route('/gallery/{nurseId}', name: 'app_gallery', methods: 'POST')]
     public function index(Request $request, int $nurseId): JsonResponse
     {
@@ -59,10 +61,23 @@ class GalleryController extends AbstractController
         return $this->json([], Response::HTTP_CREATED);
     }
 
+    #[IsGranted('ROLE_NURSE', message: 'Vous ne pouvez pas faire ça')]
     #[Route('/gallery/nurse/{nurseId}', name: 'app_gallery_get', methods: 'GET')]
     public function gallery(int $nurseId): JsonResponse
     {
         $photos = $this->galleryRepository->findBy(['nurse' => $nurseId]);
         return $this->json($photos, Response::HTTP_CREATED, [], ['groups' => 'gallery']);
     }
+
+    #[IsGranted('ROLE_NURSE', message: 'Vous ne pouvez pas faire ça')]
+    #[Route('/gallery/{galleryId}', name: 'app_gallery_delete', methods: 'DELETE')]
+    public function delete(int $galleryId): JsonResponse
+    {
+        $entityManager = $this->doctrine->getManager();
+        $photo = $this->galleryRepository->findOneBy(['id' => $galleryId]);
+        $entityManager->remove($photo);
+        $entityManager->flush();
+        return $this->json([], Response::HTTP_NO_CONTENT);
+    }
+
 }
